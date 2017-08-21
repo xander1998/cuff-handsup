df50b66c70abd597ef5a57d0cbce2aec5ea95a45
xander1998 <xander.harrison77@gmail.com>
2017-08-21 13:24:36 -0500

    fixed attacking while cuffed and while hands are up.

--- cl_cuff-hands.lua
+++ cl_cuff-hands.lua
@@ -1,23 +1,41 @@
+local handcuffconfig = {
+	model = "prop_cs_cuffs_01",
+	handcuffs = nil
+}
+
 
 --[[ HANDCUFF SCRIPT ]]--
 RegisterNetEvent("Handcuff")
 AddEventHandler("Handcuff", function()
 	local lPed = GetPlayerPed(-1)
 	if DoesEntityExist(lPed) then
-		Citizen.CreateThread(function()
+		if IsEntityPlayingAnim(lPed, "mp_arresting", "idle", 3) then
+			--DetachEntity(handcuffconfig.handcuffs, 0, 0)
+			--DeleteEntity(handcuffconfig.handcuffs)
+			--handcuffconfig.handcuffs = nil
+			ClearPedSecondaryTask(lPed)
+			SetEnableHandcuffs(lPed, false)
+			SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
+		else
 			RequestAnimDict("mp_arresting")
 			while not HasAnimDictLoaded("mp_arresting") do
 				Citizen.Wait(100)
 			end
-			
-			if IsEntityPlayingAnim(lPed, "mp_arresting", "idle", 3) then
-				ClearPedSecondaryTask(lPed)
-				SetEnableHandcuffs(lPed, false)
-			else
-				TaskPlayAnim(lPed, "mp_arresting", "idle", 8.0, -8, -1, 49, 0, 0, 0, 0)
-				SetEnableHandcuffs(lPed, true)
-			end		
-		end)
+
+			--RequestModel(GetHashKey(handcuffconfig.model))
+			--while not HasModelLoaded(GetHashKey(handcuffconfig.model)) do
+			--	Citizen.Wait(100)
+			--end
+
+			--local plyCoords = GetEntityCoords(GetPlayerPed(PlayerId()), false)
+			--handcuffconfig.handcuffs = CreateObject(GetHashKey(handcuffconfig.model), plyCoords.x, plyCoords.y, plyCoords.z, 1, 1, 1)
+
+			--AttachEntityToEntity(handcuffconfig.handcuffs, GetPlayerPed(PlayerId()), GetPedBoneIndex(GetPlayerPed(PlayerId()), 60309), 0.0, 0.05, 0.0, 0.0, 0.0, 80.0, 1, 0, 0, 0, 0, 1)
+
+			TaskPlayAnim(lPed, "mp_arresting", "idle", 8.0, -8, -1, 49, 0, 0, 0, 0)
+			SetEnableHandcuffs(lPed, true)
+			SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
+		end
 	end
 end)
 --]]
@@ -38,10 +56,12 @@ AddEventHandler("Handsup", function()
 			if IsEntityPlayingAnim(lPed, "random@mugging3", "handsup_standing_base", 3) then
 				ClearPedSecondaryTask(lPed)
 				SetEnableHandcuffs(lPed, false)
+				SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
 				TriggerEvent("chatMessage", "", {255, 0, 0}, "Your have put your hands down.")
 			else
 				TaskPlayAnim(lPed, "random@mugging3", "handsup_standing_base", 8.0, -8, -1, 49, 0, 0, 0, 0)
 				SetEnableHandcuffs(lPed, true)
+				SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
 				TriggerEvent("chatMessage", "", {255, 0, 0}, "Your hands are up.")
 			end
 		else
@@ -53,8 +73,24 @@ end)
 
 --[[---------------------]]--
 
---[[ DRAG SCRIPT ]]--
+--[[ Other Functions ]]--
 
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 3) then
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+			SetPedPathCanUseLadders(GetPlayerPed(PlayerId()), false)
+		end
 
+		if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@mugging3", "handsup_standing_base", 3) then
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+		end
+	end
+end)
 
 --]]
