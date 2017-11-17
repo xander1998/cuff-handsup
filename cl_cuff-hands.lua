1c438361ea9a67c02be38f6bfaa4f5a1543106c5
xander1998 <xander.harrison77@gmail.com>
2017-11-16 19:18:37 -0600

    Removed some code

--- cl_cuff-hands.lua
+++ cl_cuff-hands.lua
@@ -1,18 +1,9 @@
-local handcuffconfig = {
-	model = "prop_cs_cuffs_01",
-	handcuffs = nil
-}
-
-
 --[[ HANDCUFF SCRIPT ]]--
 RegisterNetEvent("Handcuff")
 AddEventHandler("Handcuff", function()
 	local lPed = GetPlayerPed(-1)
 	if DoesEntityExist(lPed) then
 		if IsEntityPlayingAnim(lPed, "mp_arresting", "idle", 3) then
-			--DetachEntity(handcuffconfig.handcuffs, 0, 0)
-			--DeleteEntity(handcuffconfig.handcuffs)
-			--handcuffconfig.handcuffs = nil
 			ClearPedSecondaryTask(lPed)
 			SetEnableHandcuffs(lPed, false)
 			SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
@@ -21,16 +12,6 @@ AddEventHandler("Handcuff", function()
 			while not HasAnimDictLoaded("mp_arresting") do
 				Citizen.Wait(100)
 			end
-
-			--RequestModel(GetHashKey(handcuffconfig.model))
-			--while not HasModelLoaded(GetHashKey(handcuffconfig.model)) do
-			--	Citizen.Wait(100)
-			--end
-
-			--local plyCoords = GetEntityCoords(GetPlayerPed(PlayerId()), false)
-			--handcuffconfig.handcuffs = CreateObject(GetHashKey(handcuffconfig.model), plyCoords.x, plyCoords.y, plyCoords.z, 1, 1, 1)
-
-			--AttachEntityToEntity(handcuffconfig.handcuffs, GetPlayerPed(PlayerId()), GetPedBoneIndex(GetPlayerPed(PlayerId()), 60309), 0.0, 0.05, 0.0, 0.0, 0.0, 80.0, 1, 0, 0, 0, 0, 1)
 
 			TaskPlayAnim(lPed, "mp_arresting", "idle", 8.0, -8, -1, 49, 0, 0, 0, 0)
 			SetEnableHandcuffs(lPed, true)
