d95fc6843a09c1e1a1f9a0e25ebc412e2e712f8b
xander1998 <xander.harrison77@gmail.com>
2017-11-16 20:35:55 -0600

    Possible fix for the random uncuffing when getting out of cars.

--- cl_cuff-hands.lua
+++ cl_cuff-hands.lua
@@ -1,3 +1,5 @@
+local handcuff = false
+
 --[[ HANDCUFF SCRIPT ]]--
 RegisterNetEvent("Handcuff")
 AddEventHandler("Handcuff", function()
@@ -7,6 +9,7 @@ AddEventHandler("Handcuff", function()
 			ClearPedSecondaryTask(lPed)
 			SetEnableHandcuffs(lPed, false)
 			SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
+			handcuff = false
 		else
 			RequestAnimDict("mp_arresting")
 			while not HasAnimDictLoaded("mp_arresting") do
@@ -16,6 +19,7 @@ AddEventHandler("Handcuff", function()
 			TaskPlayAnim(lPed, "mp_arresting", "idle", 8.0, -8, -1, 49, 0, 0, 0, 0)
 			SetEnableHandcuffs(lPed, true)
 			SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
+			handcuff = true
 		end
 	end
 end)
@@ -59,17 +63,29 @@ end)
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
+
+		if handcuff and not IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 3) then
+			Citizen.Trace("BACKUP CUFFING TRIGGERED")
+			TaskPlayAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 8.0, -8, -1, 49, 0, 0, 0, 0)
+		end
+
 		if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 3) then
 			DisableControlAction(1, 140, true)
 			DisableControlAction(1, 141, true)
 			DisableControlAction(1, 142, true)
 			SetPedPathCanUseLadders(GetPlayerPed(PlayerId()), false)
+			if IsPedInAnyVehicle(GetPlayerPed(PlayerId()), false) then
+				DisableControlAction(0, 59, true)
+			end
 		end
 
 		if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@mugging3", "handsup_standing_base", 3) then
 			DisableControlAction(1, 140, true)
 			DisableControlAction(1, 141, true)
 			DisableControlAction(1, 142, true)
+			if IsPedInAnyVehicle(GetPlayerPed(PlayerId()), false) then
+				DisableControlAction(0, 59, true)
+			end
 		end
 	end
 end)
